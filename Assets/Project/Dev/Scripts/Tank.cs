39036366aab62de79e79ff2d0ea624119b53b070
Alexej Yarotski <alex.yarotski@gmail.com>
2022-11-23 20:26:44 +0300

    Helth Tower

--- Assets/Project/Dev/Scripts/Tank.cs
+++ Assets/Project/Dev/Scripts/Tank.cs
@@ -33,8 +33,8 @@ public class Tank : MonoBehaviour
     {
         if (collision.gameObject.TryGetComponent(out Tower tower))
         {
-            bool f = _towerPrefab.HealthLevel(_damage);
-            _towerPrefab.gameObject.SetActive(f);
+            bool health = _towerPrefab.HealthLevel(_damage);
+            _towerPrefab.gameObject.SetActive(health);
             Destroy(gameObject);
         }
     }
