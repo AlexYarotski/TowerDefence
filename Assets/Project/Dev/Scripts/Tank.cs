66074b9d7bb5f469d0a6ad5baaf6b95e79a64e08
Alexej Yarotski <alex.yarotski@gmail.com>
2022-12-13 20:36:12 +0300

    Turn Tank and UI(bag)

--- Assets/Project/Dev/Scripts/Tank.cs
+++ Assets/Project/Dev/Scripts/Tank.cs
@@ -3,19 +3,17 @@ using UnityEngine;
 
 public class Tank : DamageableObject
 {
-    public static event Action<Tank> Dead = delegate {  }; 
+    public static event Action<Tank> Dead = delegate { };
 
-    [SerializeField]
-    private float _speed = 0;
+    [SerializeField] private float _speed = 0;
 
-    [SerializeField] 
-    private float _damage = 0;
+    [SerializeField] private float _damage = 0;
 
     private Transform _target = null;
 
     private void FixedUpdate()
     {
-        var finalPos = new Vector3(_target.transform.position.x, 1, _target.transform.position.z);
+        var finalPos = new Vector3(_target.transform.position.x, 5, _target.transform.position.z);
         float step = Time.deltaTime * _speed;
 
         var moveDirection = (finalPos - transform.position).normalized * step;
@@ -27,17 +25,21 @@ public class Tank : DamageableObject
         if (collision.gameObject.TryGetComponent(out Tower tower))
         {
             tower.GetDamage(_damage);
-            
+
             OnDie();
         }
     }
-    
-    public void SetTargetPosition(Transform targetTransform)
-     {
-         _target = targetTransform;
-     } 
 
-    protected override void OnDie()
+    public void SetTargetPosition(Transform targetTransform)
+    {
+        _target = targetTransform;
+
+        var rotation = Quaternion.LookRotation((transform.position - _target.position).normalized, Vector3.up).normalized;
+        transform.Rotate(0, rotation.eulerAngles.y + 180, 0);
+    }
+
+
+protected override void OnDie()
     {
         base.OnDie();
         Dead(this);
